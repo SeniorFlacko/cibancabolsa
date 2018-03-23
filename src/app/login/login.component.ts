f1e9168791342401c3ecfd0ff1ca92b66c5d49dc
Cristian Aleman <cristianalemanfuentes@gmail.com>
2018-03-23 12:17:33 -0600

    Token Screen

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { User } from '../models/user';
 import { LoginService } from '../services/login.service';
+import { Token } from '../models/token';
 
 @Component({
   selector: 'app-login',
@@ -11,26 +12,58 @@ import { LoginService } from '../services/login.service';
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
-  user: User;
+  tokenForm: FormGroup;
+  showtokenscreen:boolean;
 
   constructor(private fb: FormBuilder, private ls: LoginService) { 
-    this.createForm();
+    this.createLoginForm();
+    this.createTokenForm();
+    this.showtokenscreen = false;
   }
 
   ngOnInit() {
   }
 
 
-  createForm() {
+  createLoginForm() {
     this.loginForm = this.fb.group({
       name: ['', Validators.required ],
       password: ['', Validators.required ]
     });
   }
 
+  createTokenForm() {
+    this.tokenForm = this.fb.group({
+      value: ['', Validators.required ],
+    });
+  }
+
   onSubmit(){
-    this.user = this.loginForm.value;
-    this.ls.login(this.user)
-      .subscribe(response => console.log("Log ", response));
+    let user:User = this.loginForm.value;
+    this.ls.login(user)
+      .subscribe(response => {
+        console.log(response);
+        if(response)
+          this.showTokenScreen();
+      });
+  }
+
+  onTokenSubmit(){
+    let token:Token = this.tokenForm.value;
+    
+    this.ls.aproveToken(token)
+      .subscribe(response => {
+        if (response)
+          this.moveToHome();
+        
+      });
+  }
+
+  showTokenScreen(){
+    this.showtokenscreen = true;
+  }
+
+  moveToHome(){
+    console.log('Ir a Home');
   }
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user';
+import { Token } from '../models/token';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
@@ -12,6 +13,14 @@ export class LoginService {
   login( user:User ): Observable<boolean> {
     let response:boolean = false;
     if(user.name==="admin" && user.password==="123")
+      response = true;
+
+    return of(response);
+  }
+
+  aproveToken( token:Token ): Observable<boolean> {
+    let response:boolean = false;
+    if(token.value==="1234")
       response = true;
 
     return of(response);
