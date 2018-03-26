29a4717e1e5ef03bf1c53efd5d1967246379a3ef
Cristian Aleman <cristianalemanfuentes@gmail.com>
2018-03-26 12:44:36 -0600

    Login con mensajes de validacion y filter keydowns para no type

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -32,14 +32,14 @@ export class LoginComponent implements OnInit {
 
   createLoginForm() {
     this.loginForm = this.fb.group({
-      name: ['', Validators.required ],
+      name: ['', [Validators.required, Validators.maxLength(20)] ],
       password: ['', Validators.required ]
     });
   }
 
   createTokenForm() {
     this.tokenForm = this.fb.group({
-      value: ['', Validators.required ],
+      value: ['', [Validators.required, Validators.maxLength(8), Validators.minLength(8)] ],
     });
   }
 
@@ -71,4 +71,45 @@ export class LoginComponent implements OnInit {
   moveToHome(){
     this.router.navigate(['/home']);
   }
+
+
+  filterAlpha($event){
+    let alfaOnly = /^[a-zA-Z0-9\u00C1\u00E1\u00C9\u00E9\u00CD\u00ED\u00D3\u00F3\u00DA\u00FA\u00DC\u00FC\u00f1\u00D1]+$/;
+    if(alfaOnly.test($event.key))
+      return true
+    return false;
+  }
+  
+  pasteNumber($event) {
+    return false;
+  }
+
+  filterNumber($event) {
+    const keysToAccept = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Backspace', 'ArrowLeft', 'ArrowRight', 'Delete', 'Tab', 'Shift'];
+    
+    if(this.tokenForm.get('value').value.length>7){
+      const keysToDeleteAccept = ['Backspace', 'ArrowLeft', 'ArrowRight', 'Delete', 'Tab', 'Shift'];
+      if(keysToDeleteAccept.indexOf($event.key) !== -1)
+        return true;
+      return false;
+    }
+    if (keysToAccept.indexOf($event.key) !== -1) {
+      return true;
+    }
+    return false;
+  }
+
+
+  get name() { 
+    return this.loginForm.get('name'); 
+  }
+
+  get password() { 
+    return this.loginForm.get('password'); 
+  }
+
+  get token(){
+    return this.tokenForm.get('value');
+  }
+
 }
