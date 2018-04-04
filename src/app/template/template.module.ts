70802351aa488aa6107fd3bd837af0659d6f6757
Cristian Aleman <cristianalemanfuentes@gmail.com>
2018-04-04 11:54:18 -0500

    Slide Toggle

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -4,10 +4,10 @@ import { CommonModule } from '@angular/common';
 import { TemplateRoutingModule } from './template-routing.module';
 import { PortafolioComponent, MercadocapitalesComponent, MercadodineroComponent, FondoinversionComponent, OrdenesComponent, ReportefinancieroComponent } from './index.templatecmpts';
 import { MaterialModule } from '../material.module';
-import { OrdenesPendientesComponent } from './portafolio/ordenes-pendientes/ordenes-pendientes.component';
 import { TablaOrdenesPendientesComponent } from './portafolio/tabla-ordenes-pendientes/tabla-ordenes-pendientes.component';
 import { TablaMercadoCapitalesComponent } from './portafolio/tabla-mercado-capitales/tabla-mercado-capitales.component';
 import { TablaMercadoDineroComponent } from './portafolio/tabla-mercado-dinero/tabla-mercado-dinero.component';
+import { SlideToogleComponent } from './portafolio/slide-toogle/slide-toogle.component';
 
 @NgModule({
   imports: [
@@ -22,10 +22,10 @@ import { TablaMercadoDineroComponent } from './portafolio/tabla-mercado-dinero/t
     FondoinversionComponent,
     OrdenesComponent,
     ReportefinancieroComponent,
-    OrdenesPendientesComponent,
     TablaOrdenesPendientesComponent,
     TablaMercadoCapitalesComponent,
     TablaMercadoDineroComponent,
+    SlideToogleComponent,
 
   ]
 })
