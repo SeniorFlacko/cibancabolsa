5b501910d2f54de8426f4d1d914e045fc69669a2
Cristian Aleman <cristianalemanfuentes@gmail.com>
2018-04-04 11:02:39 -0500

    Tablas de portafolio

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { TemplateRoutingModule } from './template-routing.module';
 import { PortafolioComponent, MercadocapitalesComponent, MercadodineroComponent, FondoinversionComponent, OrdenesComponent, ReportefinancieroComponent } from './index.templatecmpts';
 import { MaterialModule } from '../material.module';
+import { OrdenesPendientesComponent } from './portafolio/ordenes-pendientes/ordenes-pendientes.component';
+import { TablaOrdenesPendientesComponent } from './portafolio/tabla-ordenes-pendientes/tabla-ordenes-pendientes.component';
 
 @NgModule({
   imports: [
@@ -18,6 +20,8 @@ import { MaterialModule } from '../material.module';
     FondoinversionComponent,
     OrdenesComponent,
     ReportefinancieroComponent,
+    OrdenesPendientesComponent,
+    TablaOrdenesPendientesComponent,
 
   ]
 })
