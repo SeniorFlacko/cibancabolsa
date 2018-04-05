1593f449474537161b42ab848b5eb878c4a065e0
Jesus Cano <jcanot1300@alumno.ipn.mx>
2018-04-04 19:01:58 -0500

    Estilos de tabla y valores del servicio, creacion de los modelos

--- src/app/models/index.models.ts
+++ src/app/models/index.models.ts
@@ -2,3 +2,4 @@ export { Token } from './token';
 export { User } from './User';
 export { OrdenPendiente } from './orden-pendiente'
 export { Capital } from './capital'
+export { Valores } from './values.model';
