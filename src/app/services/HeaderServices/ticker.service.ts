8f4e239c99914389763f462b7f9f3740709def10
Cristian Aleman <cristianalemanfuentes@gmail.com>
2018-04-11 12:09:59 -0500

    Ticker consulta cada segundo

--- src/app/services/HeaderServices/ticker.service.ts
+++ src/app/services/HeaderServices/ticker.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Ticker } from '../../models/ticker';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMapTo';
+import 'rxjs/add/observable/timer';
+
 @Injectable()
 export class TickerService {
 
@@ -13,7 +16,14 @@ export class TickerService {
   getValues():Observable<Ticker>{
     let URL= "https://cicasabolsa.firebaseio.com/valores.json";
     let headers = new Headers({'Content-Type':'application/json'});
+
+    // Obtener respuesta cada segundo
+    // return Observable.timer(0, 1000)
+    //   .flatMapTo(this.http.get(URL))
+    //   .map(res => res.json());
+    
     return this.http.get(URL).map(res=>{
+      console.log(res.json());
       return res.json();
     });
   }
