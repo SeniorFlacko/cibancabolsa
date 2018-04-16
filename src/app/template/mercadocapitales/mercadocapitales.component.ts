7b13b754f61eedbf42da7baa431dd39034eb3e83
Cristian Aleman <cristianalemanfuentes@gmail.com>
2018-04-16 12:32:38 -0500

    Mercado de capitales toggle favoritos

--- src/app/template/mercadocapitales/mercadocapitales.component.ts
+++ src/app/template/mercadocapitales/mercadocapitales.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit,ViewChild } from '@angular/core';
 import { DataSource } from '@angular/cdk/table';
 import { MatSort, MatTableDataSource, MatPaginator } from '@angular/material';
 import { MercadocapitalesservicioService } from '../../services/index.services';
+import { MercadoCapitales } from '../../models/index.models';
 
 @Component({
   selector: 'app-mercadocapitales',
@@ -10,7 +11,7 @@ import { MercadocapitalesservicioService } from '../../services/index.services';
 })
 export class MercadocapitalesComponent implements OnInit {
   displayedColumns = ['emisora', 'precio_cierre', 'precio_ultimo', 'precio_variacion','precio_compra','precio_venta','volumen','maximo','minimo','favorita'];
-  dataCapitales;
+  dataCapitales:MercadoCapitales[];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
@@ -23,6 +24,16 @@ export class MercadocapitalesComponent implements OnInit {
 
   getDataCapitales(){
     this.dataCapitales=this.dataCapService.getMercadoCapitalesData();
+  }
+
+  toggle(orden:MercadoCapitales){
+    this.dataCapitales.forEach( capital => {
+      capital.id === orden.id ? capital.favorita = !capital.favorita : null;
+    });
+  }
+
+  star_icon(star:boolean): string{
+    return star ?  "star" : "star_border";
   }
 }
 
