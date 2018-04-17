4c14b47114869907e8f43bb2c5a9533ed4fcc7f8
ALEX <aesquivel@na-at.com.mx>
2018-04-16 19:54:21 -0500

    cambios a componente reporte financiero

--- src/app/template/reportefinanciero/reportefinanciero.component.ts
+++ src/app/template/reportefinanciero/reportefinanciero.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-reportefinanciero',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reportefinanciero.component.css']
 })
 export class ReportefinancieroComponent implements OnInit {
+  chart = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.renderGraphic();
+  }
+
+
+  renderGraphic() {
+    this.chart = new Chart(document.getElementById("myChart"),
+      {"type":"line",
+        "data":{
+          "labels":["January","February","March","April","May","June","July",65,59,80,81,56,55,40,65,59,80,81,56,55,40,65,59,80,81,56,55,40],
+          "datasets":[
+              {
+              "label":"My First Dataset",
+              "data":[65,59,80,81,56,55,40,65,59,80,81,56,55,40,65,59,80,81,56,55,40,65,59,80,81,56,55,40,65,59,80,81,56,55,40,65,59,80,81,56,55,40],
+              "fill":false,
+              "borderColor":"rgb(75, 192, 192)",
+              "lineTension":0.1
+              }
+            ]
+        },
+        "options":{}
+      }
+      );
+  }
 }
