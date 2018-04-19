4f7cd9cd58aceb632087e075318463ba635563bd
Cristian Aleman <cristianalemanfuentes@gmail.com>
2018-04-19 17:57:20 -0500

    Retiro con exito

--- src/app/template/retiro/retiro.component.ts
+++ src/app/template/retiro/retiro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+declare var $ : any;
 
 @Component({
   selector: 'app-retiro',
@@ -8,14 +9,14 @@ import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 })
 export class RetiroComponent implements OnInit {
 
-  pantalla: string;
-  retiroForm: FormGroup;
-
+  private pantalla: string;
+  private retiroForm: FormGroup;
+  private ok = false;
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.createRetiroForm();
-    this.pantalla = 'Seleccionar';
+    this.showScreen('Seleccionar');
   }
 
   createRetiroForm() {
@@ -26,12 +27,38 @@ export class RetiroComponent implements OnInit {
   }
 
   onRetiroSubmit(){
-    this.pantalla='Verificar';
+    this.showScreen('Verificar');
   }
 
   retiroFormCancel(){
     this.retiroForm.reset();
   }
 
+  cancel(){
+    this.retiroForm.reset();
+    this.showScreen('Seleccionar');
+  }
+
+
+  showScreen(pantalla: string){
+    this.pantalla = pantalla;
+  }
+
+  get cuenta() { 
+    return this.retiroForm.get('cuenta').value; 
+  }
+
+  get importe() { 
+    return this.retiroForm.get('importe').value; 
+  }
+
+  showTokenValidate(){
+    $('#contrasenia-dinamica').modal('show');
+  }
+
+  onTokenValidate(){
+    this.showScreen('Comprobar');
+    $('#contrasenia-dinamica').modal('hide');
+  }
 
 }
