46cd2b598a037ba62f7ac80821e81ce3b41b2283
Cristian Aleman <cristianalemanfuentes@gmail.com>
2018-04-20 16:53:18 -0500

    Buscar por rango de fecha

--- src/app/template/historialmovimientos/historialmovimientos.component.ts
+++ src/app/template/historialmovimientos/historialmovimientos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+
+declare var $ : any;
 
 @Component({
   selector: 'app-historialmovimientos',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistorialmovimientosComponent implements OnInit {
 
-  constructor() { }
+  private historialForm: FormGroup;
+  private selecFormGroup: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.createHistorialForm();
+    this.createSelectForm();
+    this.suscribeToChanges();
+    
   }
 
+  createHistorialForm() {
+    this.historialForm = this.fb.group({
+      fechaInicio: ['', [Validators.required]],
+      fechaFin: ['', [Validators.required]],
+    });
+  }
+
+  createSelectForm(){
+    this.selecFormGroup = new FormGroup({
+      selectControl: new FormControl('x')
+    });
+  }
+
+  suscribeToChanges(){
+    this.select.valueChanges.subscribe(this.onSelectChanges);
+  }
+
+  onSelectChanges(value){
+    console.log(value);
+  }
+
+  get select() { 
+    return this.selecFormGroup.get('selectControl');
+  }
 }
