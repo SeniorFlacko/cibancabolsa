210e433a2e2e495e8b12f7b7d6b08649b166b1de
Cristian Aleman <cristianalemanfuentes@gmail.com>
2018-04-30 12:22:30 -0500

    Display buscador only in certain pages

--- src/app/shared/header/buscador/buscador.component.ts
+++ src/app/shared/header/buscador/buscador.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-buscador',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscadorComponent implements OnInit {
 
-  constructor() { }
+  private oculto: boolean = true;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+
+    this.router.events
+      .filter( (rtr:NavigationEnd) => rtr instanceof NavigationEnd)
+      .subscribe((rtr:NavigationEnd) => {
+        if (rtr.url == '/capitales' || 
+            rtr.url == '/dinero' ||
+            rtr.url == '/inversion'
+        ) {
+          this.oculto = false;
+        }
+        else{
+          this.oculto = true;
+        }
+        
+      });
+
   }
 
 }
